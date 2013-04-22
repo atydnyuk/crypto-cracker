c4e06f1a5cde54579b02200c6f95dee70046dc3c
atydnyuk <atydnyuk@gmail.com>
2013-04-22 11:55:44 -0400

    added some option parsing. need to add explicit stuff

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 crypto-cracker
 ==============
 
-Implementing various techniques for brute forcing cryptographic message. So far the plan is to solve Caeser ciphers and other substitution ciphers.
+Implementing various techniques for brute forcing cryptographic
+message. So far the plan is to solve Caeser ciphers and other
+substitution ciphers.
--- crackit.py
+++ crackit.py
@@ -4,25 +4,51 @@ import collections
 import itertools
 from collections import defaultdict
 from collections import Counter
+from optparse import OptionParser
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l',
            'm','n','o','p','q','r','s','t','u','v','w','x',
            'y','z']
 
+####
+#### OPTIONS AND FLAGS
+####
+
+# -f : Specify file
+# -e : Explicitly provide the letter substitution array
+# -q : Frequency analysis
+# -r : Rotation brute force
+
 def main():
-    f = open("caesar3.txt","r")
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename", default="caesar3.txt",
+                      help="Choose input file", metavar="FILE")
+    parser.add_option("-e","--explicit", dest="explicit", default=False,
+                      help="Explicitly specify replacement array")
+    parser.add_option("-q", "--freq", dest="freq", default=False,
+                      action="store_true",help="Use frequency analysis")
+    parser.add_option("-r", "--rot", dest="rot", default=False,
+                      action="store_true",help="Use rotation brute force")
+    (options, args) = parser.parse_args()
+
+    f = open(options.filename,"r")
     text = f.read()
     print "This is the cipher read:\n"
     print text;
+    
     text = text.lower()
     int_text = convert_to_int(text)
     basearray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,
                  14,15,16,17,18,19,20,21,22,23,
                  24,25]
-    try_rotations(int_text,basearray)
-    frequency_analysis(text,int_text)
+    
+    if (options.rot):
+        try_rotations(int_text,basearray)
+    if (options.freq):
+        frequency_analysis(text,int_text)
 
 def frequency_analysis(text,int_text):
+    #list of letters in order of frequency
     freq_list = ['e','t','a','o','i','n','s','h','r','d','l','c','u',
                  'm','w','f','g','y','p','b','v','k','j','x','q','z']
     basearray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,
@@ -33,17 +59,17 @@ def frequency_analysis(text,int_text):
         d[word] += 1
     commons = Counter(d).most_common(26)
     count = 0
-    print "COMMONS: \n"
-    print commons
+    
     for k,v in commons:
         if (k>=0 and k < 26):
-            print k
             basearray[k] = ord(freq_list[count])-97
             count+=1
-    print basearray
-    print "Possible solution\n"
+    
+    print "\nFrequency analysis decrypt\n"
     print ''.join(convert_to_text(replace_2(int_text,basearray)))
-        
+    print "\n\nThe replacements were as follows : \n"
+    print freq_list
+    print "\nReplace the letters that you want and relaunch with -e"
     
 def try_rotations(int_text,basearray):
     for i in range(26):
@@ -52,7 +78,7 @@ def try_rotations(int_text,basearray):
         decrypt_string= ''.join(text_text)
         #not exactly a perfect check...
         if ("the" in decrypt_string):
-            print "Possible decrypt\n"
+            print "Rotation decrypt\n"
             print decrypt_string
 
 def convert_to_int(text):
